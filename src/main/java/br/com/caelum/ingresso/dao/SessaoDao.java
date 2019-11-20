e86b605e195bfdc28b05d4b6a981f790c7a88bd2
Lucas Tsutsumi <lucastsutsumiramos@gmail.com>
2019-11-20 14:08:26 -0300

    adicionando a pagina de sessoes

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -6,7 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
@@ -24,5 +26,10 @@ public class SessaoDao {
 		
 		return manager.createQuery("select s from Sessao s where s.sala = :sala", Sessao.class)
 									.setParameter("sala", sala).getResultList();
+	}
+	
+	public List<Sessao> buscaSessoesDoFilme(Filme filme){
+		return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class)
+										.setParameter("filme", filme).getResultList();
 	}
 }
