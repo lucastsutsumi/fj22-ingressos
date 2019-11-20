0260ad28bcb3383290838cbac01a8bdbaabacadc
Lucas Tsutsumi <lucastsutsumiramos@gmail.com>
2019-11-20 10:39:24 -0300

    adicionando a logica e paginas de preço

--- src/main/java/br/com/caelum/ingresso/model/Filme.java
+++ src/main/java/br/com/caelum/ingresso/model/Filme.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Duration;
 
 /**
@@ -18,8 +21,8 @@ public class Filme {
     private String nome;
     private Duration duracao;
     private String genero;
-    
-    
+    private BigDecimal preco = BigDecimal.ZERO;
+
     
     /**
      * @deprecated hibernate only
@@ -28,11 +31,12 @@ public class Filme {
 
     }
     
-    public Filme(String nome, Duration duracao, String genero) {
+    public Filme(String nome, Duration duracao, String genero, BigDecimal preco) {
 		this.nome = nome;
 		this.duracao = duracao;
 		this.genero = genero;
-	}
+		this.preco = preco;
+    }
 
 	public Integer getId() {
         return id;
@@ -64,5 +68,13 @@ public class Filme {
 
     public void setGenero(String genero) {
         this.genero = genero;
+    }
+
+    public BigDecimal getPreco() {
+    	return preco.setScale(2, RoundingMode.HALF_UP);
+    }
+    
+    public void setPreco(BigDecimal preco) {
+    	this.preco = preco;
     }
 }
