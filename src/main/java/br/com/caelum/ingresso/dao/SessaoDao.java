ae0d37cce65f06d0dc50480954c9052fd698dc26
Lucas Tsutsumi <lucastsutsumiramos@gmail.com>
2019-11-21 09:30:13 -0300

    lógica da tela de seleção de lugares

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -6,8 +6,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.web.servlet.ModelAndView;
-
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
@@ -31,5 +29,9 @@ public class SessaoDao {
 	public List<Sessao> buscaSessoesDoFilme(Filme filme){
 		return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class)
 										.setParameter("filme", filme).getResultList();
+	}
+	
+	public Sessao findOne(Integer id) {
+		return manager.find(Sessao.class, id);
 	}
 }
--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,6 +3,8 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -33,6 +35,10 @@ public class Sessao {
 		this.filme = filme;
 		this.sala = sala;
 		this.preco = sala.getPreco().add(filme.getPreco());
+	}
+	
+	public Map<String, List<Lugar>> getMapaDeLugares(){
+		return sala.getMapaDeLugares();
 	}
 
 	public Integer getId() {
@@ -74,5 +80,5 @@ public class Sessao {
     public void setPreco(BigDecimal preco) {
     	this.preco = preco;
     }
-	
+    
 }
